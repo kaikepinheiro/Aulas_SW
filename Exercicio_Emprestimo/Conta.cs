1316ab061e8d4a4e88256d2778c6e2a9983174e9
Kaike Pinheiro Simplicio <100208435+projkaikepinheiro@users.noreply.github.com>
2023-04-18 12:41:17 -0300

    Add files via upload

---
+++ Exercicio_Emprestimo/Conta.cs
@@ -0,0 +1,9 @@
+namespace Exercicio_Emprestimo;
+class Conta{
+
+    public string? nomeCliente {get;set;}
+    public int numeroConta {get;set;}
+    public double saldo {get;set;}
+    public double limite {get;set;}
+}
+
---
+++ Exercicio_Emprestimo/Program.cs
@@ -0,0 +1,13 @@
+﻿namespace Exercicio_Emprestimo;
+class Program
+{
+    static void Main(string[] args)
+    {
+        Console.WriteLine("Exercício Avaliativo Emprestimo");
+        Conta conta = new Conta();
+
+        conta.nomeCliente = "Fulano de Tal"
+        conta.numeroConta = 123;
+        conta.Limite
+    }
+}
