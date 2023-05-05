f5f00cb95cf238e5cdbddfb2c4e50c2a14cc2c50
Kaike Pinheiro Simplicio <100208435+projkaikepinheiro@users.noreply.github.com>
2023-05-05 17:14:32 -0300

    Delete Exercicio_Emprestimo directory

--- Exercicio_Emprestimo/Conta.cs
+++
@@ -1,9 +0,0 @@
-namespace Exercicio_Emprestimo;
-class Conta{
-
-    public string? nomeCliente {get;set;}
-    public int numeroConta {get;set;}
-    public double saldo {get;set;}
-    public double limite {get;set;}
-}
-
